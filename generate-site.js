04502bb811d68bacbc05b7ced52aea02a8e94290
Rapee Suveeranont <iiieyes@gmail.com>
2019-03-07 18:32:52 +0700

    Support sharing for each party open graph image Add generate site script Create template files Move data to dist/ Update README.md Remove election62 directory

---
+++ generate-site.js
@@ -0,0 +1,34 @@
+const hbs = require('handlebars');
+const fs = require('fs');
+
+const template = {
+  index: hbs.compile(fs.readFileSync('./templates/index.html', 'utf8')),
+  share: hbs.compile(fs.readFileSync('./templates/share.html', 'utf8'))
+};
+
+const partySource = fs.readFileSync('./dist/data/party.json', 'utf8');
+const partyList = JSON.parse(partySource);
+const HOSTNAME = process.env.ENV === 'production'
+  ? 'https://elect.in.th/election-map'
+  : (process.env.HOSTNAME || 'http://127.0.0.1:8080');
+
+const meta = {
+  title: 'พรรคไหนส่งผู้สมัครเขตไหนบ้าง?',
+  description: 'พรรคไหน ส่งผู้สมัครเขตไหนบ้าง? เช็คข้อมูลกันได้ที่นี่',
+  keywords: 'การเมือง, เลือกตั้ง, ประชาธิปไตย, การเมืองไทย, election, politics, democracy, thai politics, visualization, infographic, interactive, data journalism',
+  hostname: HOSTNAME
+};
+
+let output
+// index page
+output = template.index(meta);
+fs.writeFileSync(`./dist/index.html`, output, 'utf8');
+
+// share pages
+partyList.forEach(party => {
+  const data = Object.assign({}, meta, {
+    party: party.name
+  });
+  output = template.share(data);
+  fs.writeFileSync(`./dist/p/${party.name}.html`, output, 'utf8');
+});
